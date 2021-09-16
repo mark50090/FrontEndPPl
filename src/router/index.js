392cba1848aeeb0d100209c61c6a6e6420787ad2
Arm <jakarinpk1@gmail.com>
2021-09-16 13:08:16 +0700

    question-answer

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ import ErrorPage from '../views/ErrorPage.vue'
 import WorkflowReport from '../views/WorkflowReport.vue'
 import WorkflowDetail from '../views/WorkflowDetail.vue'
 import WorkflowDashboard from '../views/WorkflowDashboard.vue'
-
+import QuestionForm from '../views/QuestionForm.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -79,6 +79,11 @@ export default new VueRouter({
           path: '/summary_workflow/dashboard',
           name: 'summary_workflow_dashboard',
           component: WorkflowDashboard
+        },
+        {
+          path: '/form/input',
+          name: 'question_form',
+          component: QuestionForm
         }
       ]
     },
