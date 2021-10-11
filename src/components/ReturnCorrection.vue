62339a9a9ca66ac85c659bbb977ca834974c1be4
Sureewan Panthong <sureewan.pa@inet.co.th>
2021-10-11 16:35:58 +0700

    added functions to revert the transaction.

--- src/components/ReturnCorrection.vue
+++ src/components/ReturnCorrection.vue
@@ -25,6 +25,10 @@
                   dense
                   outlined
                   hide-details
+                  v-model="selected_order"
+                  :items="revertable_order"
+                  item-text="text"
+                  item-value="value"
                 ></v-select>
               </v-col>
           </v-row>
@@ -33,7 +37,7 @@
           <v-row class="text-box-return">
             <v-spacer></v-spacer>
             <v-col class="pa-0" cols="auto" lg="auto" md="auto">
-          <v-btn color="#67C25D" dark depressed class="font-send-return">
+          <v-btn color="#67C25D" dark depressed class="font-send-return" @click="revertTransaction">
             ส่งคืน
           </v-btn>
           </v-col>
@@ -49,13 +53,87 @@ import { EventBus } from '../EventBus'
   export default {
     data: () => ({
       dialog: false,
+      transaction_id: '',
+      revertable_order: [],
+      selected_order: ''
     }),
     mounted() {
       EventBus.$on('FormReturn',this.getFormReturn)
     },
+    beforeDestroy(){
+      EventBus.$off('FormReturn')
+    },
     methods: {
-      getFormReturn(){
+      getFormReturn(data){
         this.dialog = true
+        this.revertable_order = []
+        this.transaction_id = data.transaction_id
+        data.flow_step.forEach((element,index) => {
+          if(element.status == 'Y')
+          this.revertable_order.push({text: `ลำดับที่ ${index+1}`,value: index, name: element.name})
+        });
+      },
+      async revertTransaction(){
+        try {
+          var { data } = await this.axios.put(this.$api_url + `/transaction/api/v1/returntoedit?transaction_id=${this.transaction_id}`, {
+            "step_index": this.selected_order
+          })
+          if(data.status){
+            this.$swal({
+              backdrop: false,
+              position: 'bottom-end',
+              width: '330px',
+              title: '<svg style="width:24px;height:24px" class="alert-icon" viewBox="0 0 24 24"><path fill="#67C25D" d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" /></svg><strong class="alert-title">สำเร็จ</strong>',
+              text: 'ส่งคืนแก้ไขเอกสารสำเร็จ',
+              showCloseButton: true,
+              showConfirmButton: false,
+              timer: 5000,
+              customClass: {
+              popup: 'alert-card',
+              title: 'alert-title-block',
+              closeButton: 'close-alert-btn',
+              htmlContainer: 'alert-text-block'
+              }
+            })
+          }else{
+            this.$swal({
+              backdrop: false,
+              position: 'bottom-end',
+              width: '330px',
+              title: '<svg style="width:24px;height:24px" class="alert-icon" viewBox="0 0 24 24"><path fill="#E53935" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg><strong class="alert-title">ล้มเหลว</strong>',
+              text: 'ส่งคืนแก้ไขเอกสารไม่สำเร็จ',
+              showCloseButton: true,
+              showConfirmButton: false,
+              timer: 5000,
+              customClass: {
+              popup: 'alert-card',
+              title: 'alert-title-block',
+              closeButton: 'close-alert-btn',
+              htmlContainer: 'alert-text-block'
+              }
+            })
+          }
+        } catch (error) {
+          console.log(error);
+          this.$swal({
+            backdrop: false,
+            position: 'bottom-end',
+            width: '330px',
+            title: '<svg style="width:24px;height:24px" class="alert-icon" viewBox="0 0 24 24"><path fill="#E53935" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg><strong class="alert-title">ล้มเหลว</strong>',
+            text: 'ส่งคืนแก้ไขเอกสารไม่สำเร็จ',
+            showCloseButton: true,
+            showConfirmButton: false,
+            timer: 5000,
+            customClass: {
+            popup: 'alert-card',
+            title: 'alert-title-block',
+            closeButton: 'close-alert-btn',
+            htmlContainer: 'alert-text-block'
+            }
+          })
+        }
+        this.dialog = false
+        this.$router.push('/inbox')
       }
     }
   }
