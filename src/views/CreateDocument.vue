4393c8e783118222c01b1a3f6e7587d836f9523d
pornapon.pu@inet.co.th <pornapon.pu@inet.co.th>
2021-11-09 10:02:57 +0700

    checl invalid pdf

--- src/views/CreateDocument.vue
+++ src/views/CreateDocument.vue
@@ -37,7 +37,7 @@
                     class="text-center"
                     :src="pdf_src"
                     @num-pages="updatePageCount"
-                    
+
                     @page-loaded="loaded"
                     :page="page"
                     ref="pdfComponent"
@@ -428,6 +428,11 @@ import VueDraggableResizable from 'vue-draggable-resizable'
         if ((newValue == 1 && oldValue == 2) || (newValue == 2 && oldValue == 1)) 
             this.clearTabData()
         },
+        pdf_src(val) {
+          if(val) {
+            this.checkValidPDF()
+          }
+        }
     },
     methods: {
       translate(keyword){
@@ -671,6 +676,37 @@ import VueDraggableResizable from 'vue-draggable-resizable'
               htmlContainer: 'alert-text-block'
             }
           })
+        }
+      },
+      async checkValidPDF() {
+        try {
+          var url = this.$api_url + '/transaction/api/v1/checkValidPdf'
+          var {data} = await this.axios.post(url, {
+            "pdfData": this.pdf_src.slice(28),
+          })
+          if(data.status) {
+            if(!data.data) {
+              this.pdf_src = ""
+              this.$swal({
+                backdrop: false,
+                position: 'bottom-end',
+                width: '330px',
+                title: '<svg style="width:24px;height:24px" class="alert-icon" viewBox="0 0 24 24"><path fill="#E53935" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg><strong class="alert-title">'+ this.textLang.fail+'</strong>',
+                text: "Invalid PDF",
+                showCloseButton: true,
+                showConfirmButton: false,
+                timer: 5000,
+                customClass: {
+                  popup: 'alert-card',
+                  title: 'alert-title-block',
+                  closeButton: 'close-alert-btn',
+                  htmlContainer: 'alert-text-block'
+                }
+              })
+            }
+          }
+        } catch(e) {
+          console.log(e)
         }
       },
       async addTransaction(){
