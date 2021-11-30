d48fe007b215acd99053f0bfc56e5361bd06106a
pornapon.pu@inet.co.th <pornapon.pu@inet.co.th>
2021-11-30 15:31:05 +0700

    add flow transfer

--- src/components/TransferEdit.vue
+++ src/components/TransferEdit.vue
@@ -18,14 +18,19 @@
         </v-card-title>
         <v-card-text class="pb-2 pt-3">
           <v-row align="center" class="text-box-transfer">
-                <v-text-field color="#4caf50"
+                <v-combobox color="#4caf50"
                     class="font-box-transfer"
                     :label="textLang.transfer_email"
                     placeholder="@one.th"
                     dense
                     outlined
                     hide-details
-                ></v-text-field>
+                    v-model="actor_email.thai_email" 
+                    :error="actor_email.thai_email == ''"
+                    :items="getEmail"
+                    @input.native="actor_email.thai_email=$event.srcElement.value, getEmail = []"
+                    v-on:keyup.enter="search_citizen()"
+                ></v-combobox>
           </v-row>
         </v-card-text>
         <v-card-actions class="pb-2">
@@ -37,7 +42,7 @@
                 </v-btn>
            </v-col>
            <v-col align-self="center" class="pr-0 pl-3" cols="4" lg="4" md="4">
-                <v-btn block color="#67C25D" dark depressed class="font-send-transfer">
+                <v-btn block color="#67C25D" dark depressed class="font-send-transfer" @click="transferTemplate()">
                     {{ textLang.confirm_transfer }}
                 </v-btn>
             </v-col>
@@ -58,14 +63,68 @@ import { EventBus } from '../EventBus'
     },
     data: () => ({
       dialog: false,
+      getEmail: [],
+      actor_email: {
+        thai_email: ""
+      },
+      templateOption: {}
     }),
     mounted() {
       EventBus.$on('FormTransfer',this.getFormTransfer)
     },
+    beforeDestroy() {
+      EventBus.$off('FormTransfer')
+    },
     methods: {
-      getFormTransfer(){
+      getFormTransfer(item){
+        this.templateOption = item
         this.dialog = true
-      }
+      },
+      async transferTemplate() {
+       if(typeof this.actor_email.thai_email === 'object' && this.actor_email.thai_email.value) {
+          try {
+            var url = `/template_form/api/v1/transfer_template`
+            var {data} = await this.axios.post(this.$api_url + url, {
+              template_id: this.templateOption.template_id,
+              sender_detail: this.actor_email.thai_email.value
+            } )
+            if(data.status) {
+              EventBus.$emit('getTemplateList')
+              this.dialog = false
+            }
+          } catch(e) {
+            console.log(e)
+          }
+        }
+      },
+      async search_citizen() {
+        let searchWord = this.actor_email.thai_email
+        if(typeof this.actor_email.thai_email === 'object') {
+          searchWord = this.actor_email.thai_email.text
+        }
+        try {
+          this.getEmail = []
+          var url = `/citizen/api/v1/search_citizen`
+          var {data} = await this.axios.post(this.$api_url + url, {keyword: searchWord} )
+          if(data.status) {
+            let getResult = data.result
+            for (let index = 0; index < getResult.length; index++) {
+              this.getEmail.push({text: getResult[index].citizen_data.thai_email, value: {
+                account_id: getResult[index].account_id,
+                first_name_th: getResult[index].citizen_data.first_name_th,
+                last_name_th: getResult[index].citizen_data.last_name_th,
+                first_name_eng: getResult[index].citizen_data.first_name_eng,
+                last_name_eng: getResult[index].citizen_data.last_name_eng,
+                account_title_th: getResult[index].citizen_data.account_title_th,
+                account_title_eng: getResult[index].citizen_data.account_title_eng,
+                thai_email: getResult[index].citizen_data.thai_email
+              }})
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }
   }
 </script>
